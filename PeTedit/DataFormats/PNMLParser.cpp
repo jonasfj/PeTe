41dc5691d9b403cdf55f9766419b6b5a81b661c1
Jonas Finnemann Jensen <jopsen@gmail.com>
2011-03-01 21:41:44 +0100

    Added Factory for petrinet scene and import capability

--- PeTedit/DataFormats/PNMLParser.cpp
+++ PeTedit/DataFormats/PNMLParser.cpp
@@ -9,7 +9,9 @@ void PNMLParser::parse(QIODevice *input, PetriEngine::AbstractPetriNetFactory *f
 		input->open(QIODevice::ReadOnly);
 	xml.setDevice(input);
 	this->factory = factory;
+
 	idmap.clear();
+	arcs.clear();
 
 	while(xml.readNextStartElement()){
 		if(xml.name() == "pnml"){
@@ -18,6 +20,31 @@ void PNMLParser::parse(QIODevice *input, PetriEngine::AbstractPetriNetFactory *f
 		}else
 			xml.skipCurrentElement();
 	}
+
+	//Create arcs
+	foreach(ArcEntry entry, arcs){
+		if(!idmap.contains(entry.src)){
+			qDebug()<<"Error during parse: Arc from \""<<entry.src<<"\", which is not defined";
+			continue;
+		}
+		if(!idmap.contains(entry.dst)){
+			qDebug()<<"Error during parse: Arc to \""<<entry.dst<<"\", which is not defined";
+			continue;
+		}
+		std::string src = idmap[entry.src].name.toStdString();
+		std::string dst = idmap[entry.dst].name.toStdString();
+		if(idmap[entry.src].type == Place && idmap[entry.dst].type == Transition)
+			factory->addInputArc(src, dst, entry.weight);
+		else if(idmap[entry.src].type == Transition && idmap[entry.dst].type == Place)
+			factory->addOutputArc(src, dst, entry.weight);
+		else{
+			//TODO: Consider reporting an error
+			qDebug()<<"Error duing parse: Arc from \""<<src.c_str()<<"\" to \""<<dst.c_str()<<"\" is invalid";
+		}
+	}
+
+	idmap.clear();
+	arcs.clear();
 
 	if(!wasOpen)
 		input->close();
@@ -71,7 +98,7 @@ void PNMLParser::place(){
 	//Create place
 	factory->addPlace(name.toStdString(), initialMarking, x, y);
 	//Map id to name
-	idmap[id] = {Place, name};
+	idmap[id] = NodeName(Place, name);
 }
 
 void PNMLParser::transition(){
@@ -90,7 +117,7 @@ void PNMLParser::transition(){
 	//Create transition
 	factory->addTransition(name.toStdString(), x, y);
 	//Map id to name
-	idmap[id] = {Transition, name};
+	idmap[id] = NodeName(Transition, name);
 }
 
 void PNMLParser::arc(){
@@ -107,16 +134,7 @@ void PNMLParser::arc(){
 			xml.skipCurrentElement();
 	}
 
-	//Create arc
-	std::string src = idmap[source].name.toStdString();
-	std::string dst = idmap[target].name.toStdString();
-	if(idmap[source].type == Place && idmap[target].type == Transition)
-		factory->addInputArc(src, dst, inscription);
-	else if(idmap[source].type == Transition && idmap[target].type == Place)
-		factory->addOutputArc(src, dst, inscription);
-	else{
-		//TODO: Consider reporting an error
-	}
+	arcs.append(ArcEntry(source, target, inscription));
 }
 
 void PNMLParser::position(qreal &x, qreal &y){
---
+++ PeTedit/NetItems/PetriNetSceneFactory.cpp
@@ -0,0 +1,43 @@
+#include "PetriNetSceneFactory.h"
+
+#include "PetriNetScene.h"
+#include "PlaceItem.h"
+#include "ArcItem.h"
+#include "TransitionItem.h"
+
+PetriNetSceneFactory::PetriNetSceneFactory(QUndoGroup* undoGroup, QObject* sceneParent){
+	scene = new PetriNetScene(undoGroup, sceneParent);
+}
+
+void PetriNetSceneFactory::addPlace(const std::string &name, int tokens, double x, double y){
+	PlaceItem* item = new PlaceItem(QPointF(x,y), name.c_str());
+	item->setTokens(tokens);
+	scene->addNetItem(item);
+}
+
+void PetriNetSceneFactory::addTransition(const std::string &name, double x, double y){
+	TransitionItem* item = new TransitionItem(QPointF(x,y), name.c_str());
+	scene->addNetItem(item);
+}
+
+void PetriNetSceneFactory::addInputArc(const std::string &place, const std::string &transition, int weight){
+	arcs.append({place.c_str(), transition.c_str(), weight});
+}
+
+void PetriNetSceneFactory::addOutputArc(const std::string &transition, const std::string &place, int weight){
+	arcs.append({transition.c_str(), place.c_str(), weight});
+}
+
+PetriNetScene* PetriNetSceneFactory::makeScene(){
+	foreach(ArcEntry entry, arcs){
+		NetItem* start = scene->findNetItem(entry.src);
+		NetItem* end = scene->findNetItem(entry.dst);
+		if(start && end && start->type() != end->type()){
+			ArcItem* item = new ArcItem(start, end);
+			item->setWeight(entry.weight);
+			scene->addArc(item);
+		}
+	}
+	arcs.clear();
+	return scene;
+}
