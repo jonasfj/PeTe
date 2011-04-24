8778836838390879964c3b063b3a28423593164a
Jonas Finnemann Jensen <jopsen@gmail.com>
2011-04-24 16:06:48 +0200

    Refactored scaling

--- PeTedit/NetItems/PetriNetView.cpp
+++ PeTedit/NetItems/PetriNetView.cpp
@@ -1,8 +1,11 @@
 #include "PetriNetView.h"
 
 #include <QDebug>
+#include <QTransform>
 
 PetriNetView::PetriNetView(QWidget *parent) : QGraphicsView(parent) {
+	minScale = 0.1;
+	maxScale = 100;
 }
 
 
@@ -11,11 +14,34 @@ void PetriNetView::wheelEvent(QWheelEvent* event){
 		QGraphicsView::ViewportAnchor anchor = transformationAnchor();
 		setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
 		if(event->delta() > 0)
-			scale(1.1, 1.1);
+			scaleBy(1.1);
 		else
-			scale(1.0/1.1, 1.0/1.1);
+			scaleBy(1.0/1.1);
 		setTransformationAnchor(anchor);
 		event->accept();
 	}else
 		QGraphicsView::wheelEvent(event);
+}
+
+void PetriNetView::scaleBy(qreal factor){
+	//If we're outside the range, abort
+	if(((currentScale() == minScale) && (factor < 1.0)) ||
+	   ((currentScale() == maxScale) && (factor > 1.0)))
+		return;
+
+	if (currentScale() * factor < minScale && factor < 1.0)
+		factor = minScale / currentScale();
+	else if(currentScale() * factor > maxScale && factor > 1.0)
+		factor = maxScale / currentScale();
+
+	scale(factor, factor);
+
+	// Changed scale factor
+	emit zoomChanged(currentScale() * 100);
+}
+
+void PetriNetView::setZoom(int percent){
+	qreal targetScale = (qreal)percent / 100.0;
+	qreal factor = targetScale / currentScale();
+	scaleBy(factor);
 }
--- PeTedit/NetItems/PetriNetView.h
+++ PeTedit/NetItems/PetriNetView.h
@@ -8,12 +8,16 @@ class PetriNetView : public QGraphicsView
 {
     Q_OBJECT
 public:
-    explicit PetriNetView(QWidget *parent = 0);
-
+	explicit PetriNetView(QWidget *parent = 0);
+	qreal currentScale() const { return transform().m11(); }
+	void scaleBy(qreal factor);
+	void setZoom(int percent);
 signals:
-
+	void zoomChanged(int zoom);
 public slots:
 
+private:
+	qreal minScale, maxScale;
 protected:
 	void wheelEvent(QWheelEvent *event);
 };
