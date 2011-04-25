14ca72e38c99a7345582cb44bda9dafcb9792775
Lars Kærlund Østergaard <loern_it@hotmail.com>
2011-04-25 11:33:07 +0200

    made a simpler printer for DTAPN

---
+++ PetriEngine/DTAPN/DTAPNPrinter.h
@@ -0,0 +1,26 @@
+#ifndef DTAPNPRINTER_H
+#define DTAPNPRINTER_H
+
+#include "AbstractDTAPNBuilder.h"
+
+namespace PetriEngine{
+namespace DTAPN{
+
+
+class DTAPNPrinter : public AbstractDTAPNBuilder
+{
+public:
+    DTAPNPrinter();
+	void addPlace(const std::string& name, int tokens, double, double);
+
+	void addTransition(const std::string& name, double, double);
+
+	void addInputArc(const std::string& place, const std::string& transition, int startInterval, int endInterval);
+
+	void addOutputArc(const std::string& transition, const std::string& place);
+};
+
+} //DTAPN
+} //PetriEngine
+
+#endif // DTAPNPRINTER_H
